66a4378f2aca809c929e0415548ce633042df9c7
Akash Sarada <akashsarada@gmail.com>
2026-07-22 12:08:43 -0700

    fix(nvim): use ensure_installed (mason-lspconfig v2 dropped auto_install)

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -25,7 +25,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         lazy = false,
         opts = {
-            auto_install = true,
+            ensure_installed = { "lua_ls", "ts_ls", "jsonls", "clangd" },
         },
     },
     {
